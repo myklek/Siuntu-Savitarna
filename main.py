2663006a9ebc95ea71f7a7b5e01fb604950f61ca
Mykolas <mykolas2000@gmail.com>
2024-03-19 22:20:32 +0200

    sending data via websocket

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from weight_service import weight_queue
-# import queue
-
-#
-# def get_current_weight():
-#     try:
-#         return weight_queue.get(block=False)  # Get weight without blocking
-#     except queue.Empty:
-#         return None
+from weight_service import WeightService
+import websockets
+import asyncio
 
 
-def main():
-    print("Hello World!")
-#     wait for user to type weight wit and then print weight value
+async def init_websocket():
+    uri = "ws://192.168.1.82:4040/ws"
+    websocket = await websockets.connect(uri, ping_interval=None, ping_timeout=None)
+    return websocket
 
+
+async def send_weight(websocket, weight):
+    await websocket.send(str(weight))
+
+
+async def main():
+    websocket = await init_websocket()
+    weight_service = WeightService()
+
+    while True:
+        sleep_time = 0.2
+        await asyncio.sleep(sleep_time)
+        weight = weight_service.get_weight()
+        print("Weight: ", weight)
+        await send_weight(websocket, weight)
 
 
 if __name__ == "__main__":
-    main()
-
+    print("Hello World!")
+    asyncio.run(main())
--- weight_service.py
+++ weight_service.py
@@ -1,43 +1,16 @@
-import threading
-import time
-import sys
-import RPi.GPIO as GPIO
 from hx711v0_5_1 import HX711
-import queue
 
-hx = HX711(5, 6)
+class WeightService:
+    def __init__(self):
+        self.hx = HX711(5, 6)
+        self.hx.setReadingFormat("MSB", "MSB")
+        self.hx.autosetOffset()
+        self.offsetValue = self.hx.getOffset()
+        self.referenceUnit = 1051
+        self.hx.setReferenceUnit(self.referenceUnit)
 
-current_weight = 0.0
-weight_queue = queue.Queue()
-
-hx.setReadingFormat("MSB", "MSB")
-hx.autosetOffset()
-offsetValue = hx.getOffset()
-referenceUnit = 1051
-hx.setReferenceUnit(referenceUnit)
-
-
-def get_weight():
-    raw_bytes = hx.getRawBytes()
-    weight_value = hx.rawBytesToWeight(raw_bytes)
-    weight_value = -round(weight_value, 1)
-    return weight_value
-
-
-def read_sensor():
-    global current_weight
-    while True:
-        try:
-            current_weight = get_weight()
-            weight_queue.put(current_weight)
-            print("Weight: ", current_weight)
-
-        except (KeyboardInterrupt, SystemExit):
-            GPIO.cleanup()
-            print("[INFO] 'KeyboardInterrupt Exception' detected. Cleaning and exiting...")
-            sys.exit()
-
-
-background_thread = threading.Thread(target=read_sensor())
-background_thread.daemon = True  # Ensure it exits with the main program
-background_thread.start()
+    def get_weight(self):
+        raw_bytes = self.hx.getRawBytes()
+        weight_value = self.hx.rawBytesToWeight(raw_bytes)
+        weight_value = -round(weight_value, 1)
+        return weight_value
