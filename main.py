c41d359774f1cb356aa68f5284ec005208a20c85
Mykolas <mykolas2000@gmail.com>
2024-03-24 07:50:01 +0200

    changed to fastAPI

--- main.py
+++ main.py
@@ -1,47 +1,21 @@
+from fastapi import FastAPI, WebSocket
+
 from weight_service import WeightService
-import websockets
-import asyncio
+
+app = FastAPI()
+weight_service = WeightService()
 
 
-WEIGHT_CHANGE_THRESHOLD = 0.2
-STABLE_READINGS_REQUIRED = 5
-async def init_websocket():
-    # uri = "ws://192.168.1.122:4040/ws"
-    uri = "ws://192.168.1.82:4040/ws"
-    websocket = await websockets.connect(uri, ping_interval=None, ping_timeout=None)
-    return websocket
-
-
-async def send_weight(websocket, weight):
-    await websocket.send(str(weight))
-
-
-async def main():
-    websocket = await init_websocket()
-    weight_service = WeightService()
-
-    last_weight = weight_service.get_weight()
-    stable_readings = 0
-
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    await websocket.send_json({"status": "connected"})
     while True:
-        sleep_time = 0.1
-        await asyncio.sleep(sleep_time)
-        weight = weight_service.get_weight()
-        print("Weight: ", weight)
-        if abs(weight - last_weight) < WEIGHT_CHANGE_THRESHOLD:
-            stable_readings += 1
+        data = await websocket.receive_text()
+        if data == "getStableWeight":
+            temp_weights = weight_service.get_stable_weight()
+            for weight in temp_weights:
+                await websocket.send_json({"weight": weight})
+            await websocket.send_json({"stableWeight": weight})
         else:
-            stable_readings = 0
-
-            # If the weight has been stable for the required number of readings, send it
-        if stable_readings >= STABLE_READINGS_REQUIRED:
-            print("Weight has stabilized. Sending weight...")
-            await send_weight(websocket, weight)
-            stable_readings = 0
-
-        last_weight = weight
-
-
-if __name__ == "__main__":
-    print("Hello World!")
-    asyncio.run(main())
+            await websocket.send_text(f"Message text was: {data}")
--- weight_service.py
+++ weight_service.py
@@ -1,7 +1,15 @@
+from time import sleep
+
 from hx711v0_5_1 import HX711
+
+WEIGHT_CHANGE_THRESHOLD = 1
+STABLE_READINGS_REQUIRED = 5
+MINIMUM_WEIGHT = 5
+
 
 class WeightService:
     def __init__(self):
+        print("WeightService init")
         self.hx = HX711(5, 6)
         self.hx.setReadingFormat("MSB", "MSB")
         self.hx.autosetOffset()
@@ -14,3 +22,20 @@ class WeightService:
         weight_value = self.hx.rawBytesToWeight(raw_bytes)
         weight_value = abs(round(weight_value, 0))
         return weight_value
+
+    def get_stable_weight(self):
+        prev_weight = None
+        equal_readings = 0
+        while True:
+            sleep(0.2)
+            current_weight = self.get_weight()
+            yield current_weight  # yield the current weight reading
+            if (prev_weight is not None
+                    and abs(prev_weight - current_weight) < WEIGHT_CHANGE_THRESHOLD
+                    and current_weight > MINIMUM_WEIGHT):
+                equal_readings += 1
+                if equal_readings == STABLE_READINGS_REQUIRED:
+                    break
+            else:
+                equal_readings = 0
+            prev_weight = current_weight
